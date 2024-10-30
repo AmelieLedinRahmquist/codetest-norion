1ca66b2754947f8a5ac96b71a165c92c6c278590
Amelie Ledin Rahmquist <amelie.ledinrahmquist@solita.fi>
2024-10-30 09:51:27 +0100

    Created classes for all vehicles

---
+++ TollFeeCalculator/Models/Vehicle/Tractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator.Models.Vehicle
+{
+    public class Tractor : IVehicle
+    {
+        public string GetVehicleType()
+        {
+            return "Tractor";
+        }
+    }
+}
